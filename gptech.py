ca3377eddff34e969ef8bfe6f56c44f0a88d6932
Hugo Rodrigo Baigorria <hbaigorria@technisys.com>
2023-07-02 12:31:28 -0300

    change pipeline

--- gptech.py
+++ gptech.py
@@ -1,38 +1,45 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jun 30 15:59:15 2023
+Created on Sat Jul  1 06:07:15 2023
 
 @author: hbaigorria
 """
 
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-import sys
-from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
+from curtsies.fmtfuncs import red, bold, green, on_blue, yellow, blue, cyan
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 model_name = 'EleutherAI/gpt-neo-2.7B'
-# Obtenemos el texto pasado como parámetro
-prompt = sys.argv[1]
-model_path = '/gptech_model'
-tokenizer_path = '/gptech_tokenizer'
+
 # Descargar el modelo y el tokenizador si aún no están disponibles
-if not os.path.exists(model_path):
+if not os.path.exists('gptech_model'):
     model = AutoModelForCausalLM.from_pretrained(model_name)
-    model.save_pretrained(model_path)
+    model.save_pretrained('gptech_model')
 else:
-    model = AutoModelForCausalLM.from_pretrained(model_path)
+    model = AutoModelForCausalLM.from_pretrained('gptech_model')
 
-if not os.path.exists(tokenizer_path):
+if not os.path.exists('gptech_tokenizer'):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    tokenizer.save_pretrained(tokenizer_path)
+    tokenizer.save_pretrained('gptech_tokenizer')
 else:
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+    tokenizer = AutoTokenizer.from_pretrained('gptech_tokenizer')
 
-# Crear el generador utilizando el modelo y el tokenizador
-generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
+# Cargar el modelo y el tokenizador
+model = AutoModelForCausalLM.from_pretrained('gptech_model')
+tokenizer = AutoTokenizer.from_pretrained('gptech_tokenizer')
 
-# prompt = "crea un alert en javascript"
-res = generator(prompt, max_length=500, do_sample=True, temperature=0.7)
+prompt = "crea un componente base en reactJS con un formik para login"
 
-print(res[0]['generated_text'])
+# Tokenizar el prompt
+input_ids = tokenizer.encode(prompt, return_tensors="pt")
+
+# Generar texto utilizando el modelo
+output = model.generate(input_ids, max_length=500, do_sample=True, temperature=0.7)
+
+# Decodificar el texto generado
+generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+
+print(generated_text)
